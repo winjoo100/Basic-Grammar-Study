1ede561ed7462accab6fa2322a2a7f943ee86186
BackSeungJoo <lueun59@gmail.com>
2023-05-30 18:00:07 +0900

    함수에 대한 설명. 두수의 합 & 세수의 곱 등 실습내용

--- TestConsole001/TestConsole001.cpp
+++ TestConsole001/TestConsole001.cpp
@@ -1,20 +1,62 @@
 ﻿// TestConsole001.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
-#include <iostream>
+#include <iostream> // #include는 전처리기 지시문 (#이 있으면 전처리기 지시문이구나 라고 생각하면 된다.)
+#include <stdio.h>  // #include 안에 <, > 적어주는 .h 는 헤더파일, 추가로 <, > 안에는 주석을 달 수 없다.
+
+
+void MyNewPrint(int number)                         // 함수를 만들어 보자. (1. 함수의 정의)
+{
+    printf("입력 값은 : %d 입니다. \n\n", number);
+}
+
+// 두 개의 수의 합을 구하는 함수
+int PlusTwoNumbers(int number1, int number2)        
+{
+    printf("입력 값은 : %d, %d 입니다. \n", number1, number2);
+    return number1 + number2;
+}
+
+// 미션! 세 개의 수의 곱을 구하는 함수
+int MulThreeNumbers(int number1, int number2, int number3)
+{
+    printf("입력 값은 : %d, %d, %d, 입니다 \n", number1, number2, number3);
+    return number1 * number2 * number3;
+}
 
 int main()
 {
+    int number = 10;
+
+    printf("Hello Wolrd! \" \t\t\t");
     std::cout << "Hello World!\n";
+    printf("Hello Wolrd! %d \n\n", number - 2);
+    
+    MyNewPrint(100);                                //실제로 함수를 사용하는 줄에서 함수를 부른다. call (2. 함수의 호출)
+                                                    //100이란 값을 전달합니다. (3. 인자의 전달)
+
+    printf("두 숫자의 합은 %d 입니다. \n\n", PlusTwoNumbers(5, 7));
+
+    printf("세 숫자의 곱은 %d 입니다. \n\n", MulThreeNumbers(10, 20, 30));
+
+
+
+    // Coment 주석 이 라인은 프로그램에 영향을 주지 않는다.
+    // 내가 필요한 글자를 마음대로 적으면 된다.
+
+    // 한줄주석이라고 한다.
+
+    /*
+    * 이걸 여러줄 주석이라고 한다.
+    * 여러줄주석
+    * 이 라인들은 프로그램에 영향을 주지 않는다.
+    */
+
+    //ctrl + l 라인 지우기, ctrl + 우클릭 단어 한글자 점프, 라인아무대서나 ctrl+c, ctrl+v 하면 라인 통째 복사가능
+    //쉬프트 알트 + 아래방향키 => 여러줄을 똑같이 작성할 수 있음.
+
+    //이스케이프 시퀀스 \n
+    //변수와 함수를 구분하는 방법은 패턴을 잘 봐야함. ex(괄호가 있고 없고...)
+
+    return 0;   // 리턴을 만나는 순간 함수는 종료.
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.

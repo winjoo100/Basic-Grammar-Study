338a3ae759249193b44ee952bdee53dcf5537238
BackSeungJoo <lueun59@gmail.com>
2023-05-31 10:11:34 +0900

    📰 Init: "추가 정보를 출력하는 함수"
    이름 나이 전화번호 출력하는 함수

---
+++ TestConsole2/TestConsole2.cpp
@@ -0,0 +1,16 @@
+﻿// 이름, 나이, 전화번호 출력하는 함수
+
+#include <iostream>
+
+int Age(int age)
+{
+    printf("나이 : %d세 \n", age);
+    return age;
+}
+
+int main()
+{
+    printf("이름 : 백승주 \n");
+    Age(20);
+    printf("전화번호 : 010-1234-5678 \n");
+}

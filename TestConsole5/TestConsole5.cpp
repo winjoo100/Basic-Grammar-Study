971105c14a6c12c55168b290d8b040c5e63df46f
BackSeungJoo <lueun59@gmail.com>
2023-05-31 15:12:19 +0900

    📰 Init: "힐을 하는 함수"
    최대체력 50, 현재체력 20 일 경우
    삼항 연산자를 사용해보기

---
+++ TestConsole5/TestConsole5.cpp
@@ -0,0 +1,26 @@
+﻿// 최대체력 = 50, 현재체력 = 20 인 상태에서 힐을 하는 함수
+
+#include <stdio.h>
+
+int DoHeal(int current_Hp, int heal)
+{
+	return current_Hp + heal;
+}
+
+int main()
+{
+	int maxHp = 50;
+	int current_Hp = 20;
+
+	printf("힐 받기 전 체력 : %d\n", current_Hp);
+
+	int heal = 10000;
+
+	int doHeal = DoHeal(current_Hp, heal);
+
+	current_Hp = doHeal > maxHp ? maxHp : doHeal;
+
+
+	printf("힐량 : %d\n", heal);
+	printf("현재 체력 : %d\n", current_Hp);
+}

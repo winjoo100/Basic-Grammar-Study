0d372ac9261f0d1401c13b12e7415e0cd9cf2b62
BackSeungJoo <lueun59@gmail.com>
2023-05-31 11:49:53 +0900

    📰 Init: "크리티컬 데미지 출력"
    데미지를 입력받아서 크리티컬 히트 데미지를 출력하는 함수
    크리티컬 데미지 배수는 150%

---
+++ TestConsole3/TestConsole3.cpp
@@ -0,0 +1,14 @@
+﻿// 데미지를 입력받아서 크리티컬 히트 데미지를 출력하는 함수
+// 크리티컬 히트는 150 %
+
+#include <iostream>
+
+float CritDamage(float damage)
+{
+    return damage * 1.5;
+}
+
+int main()
+{
+    printf("크리티컬 데미지 : %f \n", CritDamage(100));
+}

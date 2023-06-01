66b5db19e927dfce159147ffb4975ce8a90c934c
BackSeungJoo <lueun59@gmail.com>
2023-06-01 16:49:35 +0900

    📰 Init: "1~10까지 출력하는 프로그램"
    while문을 이용해 1~10까지 출력해보기

---
+++ TestConsole11/TestConsole11.cpp
@@ -0,0 +1,16 @@
+﻿
+// 1 ~ 10까지 while문으로 출력해보기
+
+#include <iostream>
+
+int main()
+{
+	int loopCount = 1;
+
+	while (loopCount <= 10)
+	{
+		printf("Hello World! 몇 번째 돌고 있는지? %d \n", loopCount);
+		loopCount += 1;
+	}		// loop: 루프 종료
+
+}		//main 함수 여기서 종료

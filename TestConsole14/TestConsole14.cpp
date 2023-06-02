36d3bddd5abfa6236452a8d8dd01bf23b68edf8b
BackSeungJoo <lueun59@gmail.com>
2023-06-02 11:54:12 +0900

    📰 Init: "switch / case 문 사용해보기"
    // 추가로 conio.h 헤더파일의 _getch() 함수 사용해보기
    
    _getch() 함수는 키 입력을 받을 때까지 프로그램을 일시적으로 멈추게 하고, 사용자가 키를 누를 때까지 대기합니다.
    사용자가 키를 누르면 해당 키에 해당하는 ASCII 코드 값을 반환합니다.
    이 함수는 입력된 키를 화면에 에코하지 않으므로 입력 내용이 보이지 않습니다.

---
+++ TestConsole14/TestConsole14.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>					
+#include <conio.h>			// 
+
+int main()
+{
+	// Switch / case 문
+	char userInput = 0;
+
+	printf("User Input : ");
+	userInput = _getch();
+
+	printf("%c", userInput);
+	printf("\n\n");
+
+	//scanf_s("%d", &userInput);
+
+	switch (userInput)
+	{
+	case 'a':
+		printf("이것은 가위 \n");
+		break;
+
+	case 'b':
+		printf("이것은 바위 \n");
+		break;
+
+	case 'c':
+		printf("이것은 보 \n");
+		break;
+
+	default:
+		printf("처리되지 않은 예외 입력입니다. \n");
+		break;
+	}
+
+	printf("아무키나 누르면 프로그램을 종료합니다. \n");
+	_getch();
+}

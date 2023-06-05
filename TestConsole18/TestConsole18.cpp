dc66c0760b54cc85ef12623cbaa422b20bc43b05
BackSeungJoo <lueun59@gmail.com>
2023-06-05 18:42:06 +0900

    📰 Init: "배열 활용해서 * * * * * 별 찍기 "
    * * * * *  에서
    엔터를 누를 때 마다 0이 한 칸 씩 뒤로 밀립니다.
    
    0 * * * * -> * 0 * * * -> * * 0 * *

---
+++ TestConsole18/TestConsole18.cpp
@@ -0,0 +1,35 @@
+﻿// * * * * *
+// 0 * * * *	-> 엔터를 누를 때 마다 0이 한 칸 씩 뒤로 밀립니다.
+// - > * 0 * * *
+
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+
+int main()
+{
+	char board[5] = { 0, };
+
+	// * * * * * 찍기
+	for (int a = 0; a < 5; a++)
+	{
+		printf("%c ", board[a]);
+	}
+
+	// 0 * * * * 에서 오른쪽으로 0이 한칸씩 이동하기
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			board[j] = '*';
+			if (j == i)
+			{
+				board[j] = '0';
+			}
+			printf("%c ", board[j]);
+		}
+
+		_getch();
+		system("cls");
+	}
+}
